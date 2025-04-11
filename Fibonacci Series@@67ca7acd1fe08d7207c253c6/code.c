fca26becedcd799958f31b1b9cc979136480b2ab
HIMANSHU-uwu <raj1988ku@gmail.com>
2025-04-11 15:26:27 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -4,6 +4,8 @@ int fibonacciSeries(int n)
 {
     if(n==0) return 0;
     if(n==1) return 1;
-    else return fibonacciSeries(n-1) + fibonacciSeries(n-2);
+    for(int i=1;i<=n;i++){
+        fibonacciSeries(n-1)+fibonacciSeries(n-2);
+    }
 }
 
